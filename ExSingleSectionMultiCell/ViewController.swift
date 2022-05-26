b2b3b7d7dde62c7d17763a5ca853511edeb29bf4
jake.k <jake.k@hpcnt.com>
2022-05-26 14:23:04 +0900

    Initial Commit

---
+++ ExSingleSectionMultiCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExSingleSectionMultiCell
+//
+//  Created by Jake.K on 2022/05/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
